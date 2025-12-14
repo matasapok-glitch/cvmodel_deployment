d7a341d36c6be76092d8f1222d8a0a83932bcd34
Matmanas <matasapok@gmail.com>
2025-12-14 18:05:24 +0200

    ad

--- main.py
+++ main.py
@@ -297,4 +297,5 @@ def download_chart(filename: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
